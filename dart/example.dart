af7b32852190392fc6896d7cd2bed0f14b7f26d0
jlpereira <jorge.pereira.dev@gmail.com>
2020-08-18 23:42:28 -0300

    Move Files to Google Drive Folder

---
+++ dart/example.dart
@@ -0,0 +1,50 @@
+//Funções Dart
+soma(a, b) {
+  return a + b;
+}
+
+main() {
+//Variáveis
+//Git
+
+  //Tipos Primitivos
+  int numb = 1;
+  double doub = 1.2;
+  String stri = 'Jorge Pereira ';
+  bool boole = true;
+
+  // Inferencia
+  var inf = 'ABC';
+
+  //Estruturas
+  //Lista
+  var nomes = ['ABC', 'DEF', 'GHI'];
+  var conju = {'ABC', 'DEF', 'GHI'};
+
+  //Forçar tipo conjunto
+  Set<String> conj = {'ABC', 'DEF', 'GHI'};
+
+  Map<String, int> notas = {
+    'Ana': 9,
+    'Lucas': 8,
+  };
+
+  //Operações com Estruturas
+  nomes.add('JKL');
+
+  //Ler Listas
+  for (var chave in notas.keys) {
+    print('key = $chave');
+  }
+
+  print("  ");
+
+  for (var registro in notas.entries) {
+    print('${registro.key} = ${registro.value}');
+  }
+
+  //Executando função
+  final r = soma(1, 5);
+
+  print("O valor é: $r ");
+}
---
+++ dart/example2oo.dart
@@ -0,0 +1,41 @@
+imprimirProduto(int qnt, {String nome, double preco}) {
+  for (var i = 0; i < qnt; i++) {
+    print("O produto ${nome} tem preço R\$ ${preco}!");
+  }
+}
+
+class Produto {
+  String nome;
+  double preco;
+
+// Construtor com Parâmetro Posicional
+  //Produto(this.nome, this.preco);
+
+// Construtor com Parâmetro Nomeado
+  Produto({this.nome, this.preco = 9.99});
+
+/* Construtor Simples
+  Produto(String nome, double preco) {
+    this.nome = nome;
+    this.preco = preco;
+  }
+*/
+
+}
+
+main() {
+  //Parâmetro Posicional
+  //var p1 = new Produto('Lápis', 4.99);
+
+  //Parâmetro Nomeado
+  var p1 = new Produto(nome: 'Lápis');
+
+  /* Parâmetro Simples
+  p1.nome = 'Lápis';
+  p1.preco = 4.59;
+  */
+
+  print("O produto ${p1.nome} tem preço de: ${p1.preco}");
+
+  imprimirProduto(4, preco: 5, nome: 'Caneta');
+}
