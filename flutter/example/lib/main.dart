5c104ba0fa1c4cef898e036a1ab6e7b792b0e095
jlpereira <jorge.pereira.dev@gmail.com>
2020-08-19 01:03:41 -0300

    Final 19.08.2020

--- flutter/example/lib/main.dart
+++ flutter/example/lib/main.dart
@@ -5,8 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
