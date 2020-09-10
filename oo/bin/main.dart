08a6eb87a0ff3d17a22ed4d9adbe1e9cbfd7341e
jlpereira <jorge.pereira.dev@gmail.com>
2020-09-09 23:53:22 -0300

    Commit Codes Dart OO

---
+++ oo/bin/main.dart
@@ -0,0 +1,9 @@
+//import 'package:oo/oo.dart' as oo;
+import 'dart:io';
+import 'src/person.dart';
+
+main() {
+  Person person = Person(name: "Jorge", age: 22, sex: "M");
+
+  print(person.name);
+}
---
+++ oo/bin/src/person.dart
@@ -0,0 +1,17 @@
+//import 'dart:io';
+
+class Person {
+  String name;
+  int age;
+  String sex;
+
+  //Constructor
+  Person({this.name, this.age, this.sex});
+
+/*  {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+*/
+}
